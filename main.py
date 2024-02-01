8b367473dbee24f6d0733466ba0ab4d8f54e349b
guilherme <vinespedro55@gmail.com>
2024-02-01 15:02:57 -0300

    Update font size and position in main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import pandas as pd
 def create_canvas(textos):
     packet = io.BytesIO()
     can = canvas.Canvas(packet, pagesize=A4)
-    can.setFont("Helvetica", 11)
+    can.setFont("Helvetica", 10)
     for key, value in textos.items():
         if pd.notna(value):
             textos[key] = str(value)
@@ -34,63 +34,61 @@ def fill_pdf_template(textos, output_path):
 
     # Página 1
     can.drawString(150, 665, textos['nome'])
-    can.drawString(280, 630, textos['setor'])
-    can.drawString(37, 614, textos['cargo'])
-    can.drawString(43, 574, textos['superior_imediato'])
-    can.setFont("Helvetica", 8)
-    can.drawString(120, 552, textos['local_trabalho'])
-    can.setFont("Helvetica", 11)
-    can.drawString(362, 550, textos['ch'])
+    can.drawString(245, 618, textos['setor'])
+    can.drawString(33, 618, textos['cargo'])
+    can.drawString(33, 574, textos['superior_imediato'])
+    can.setFont("Helvetica", 10)
+    can.drawString(120, 547.5, textos['local_trabalho'])
+    can.setFont("Helvetica", 10)
+    can.drawString(360, 547.5, textos['ch'])
     lines = textwrap.wrap(textos['objetivo_cargo'], width=98)
     match textos['regime_contratacao']:
         case 'Comissionado':
-            can.drawString(324.6, 584, '\n')
+            can.drawString(324.6, 591, '\n')
         case 'Seletivo':
-            can.drawString(424, 584, '\n')
+            can.drawString(424, 591, '\n')
         case 'Efetivo':
-            can.drawString(494, 584, '\n')
-    y = 519
+            can.drawString(494, 591, '\n')
+    y = 508
     for line in lines:
-        can.drawString(34, y, line)
+        can.drawString(33, y, line)
         y -= 13
     y = 320
     for i in range(1, 8):
         if textos[f'descricao_sumaria_0{i}']:
-            can.drawString(35, y, " • "+textos[f'descricao_sumaria_0{i}'])
+            can.drawString(33, y, " • "+textos[f'descricao_sumaria_0{i}'])
         y -= 15
     can.showPage()  # Finaliza a página 1 e começa uma nova página
 
     # Página 2
     y=640
-    can.setFont("Helvetica", 11)
+    can.setFont("Helvetica", 10)
     for i in range (1,6):
         if textos[f'descricao_compet_comportamentais_0{i}']:
-            can.drawString(35, y, " • "+textos[f'descricao_compet_comportamentais_0{i}'])
+            can.drawString(33, y, " • "+textos[f'descricao_compet_comportamentais_0{i}'])
         y -= 15
 
     y= 730
     can.showPage()  # Finaliza a página 2 e começa uma nova página
-    can.showPage()  # Finaliza a página 3 e começa uma nova página
+    can.setFont("Helvetica", 10)
     for i in range(1,7):
         if textos[f'descricao_compet_tecnica_0{i}']:
-            can.drawString(35, y, " • "+textos[f'descricao_compet_tecnica_0{i}'])
+            can.drawString(33, y, " • "+textos[f'descricao_compet_tecnica_0{i}'])
         y -= 15
 
     match  textos['Escolaridade']:
         case 'Nível Fundamental':
-            can.drawString(112.2, 525, '\n')
+            can.drawString(112.2, 520, '\n')
         case 'Nível Médio':
-            can.drawString(210.3, 525, '\n')
+            can.drawString(210.3, 520, '\n')
         case 'Nível Superior':
-            can.drawString(345, 525, '\n')
+            can.drawString(345, 520, '\n')
         case 'Nível Pós-graduação':
-            can.drawString(424, 525, '\n')
-    can.drawString(328, 470, textos['habilidades_especiais'])
-    can.setFont("Helvetica", 9)
-    can.drawString(177, 419, textos['experiencia'])
+            can.drawString(424, 520, '\n')
+    can.drawString(324, 470, textos['habilidades_especiais'])
+    can.setFont("Helvetica", 9.5)
+    can.drawString(171, 414, textos['experiencia'])
 
-
-    
 
     can.save()
     write_pdf(existing_pdf, packet, output_path)
